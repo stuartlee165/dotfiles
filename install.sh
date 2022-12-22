ac78f2f9c50abe9800166c8e76eab400c0ade90f
stuartlee165 <stuartlee165@outlook.com>
2022-12-22 20:42:39 +0000

    Add Zsh config

--- install.sh
+++ install.sh
@@ -31,3 +31,12 @@ ln -s "$HOME/dotfiles/X11" "$HOME/.config"
 rm -rf "$HOME/.config/i3"
 # Create a symlink targetting the whole i3 directory
 ln -s "$HOME/dotfiles/i3" "$HOME/.config"
+
+
+#######
+# zsh #
+#######
+
+mkdir -p "$HOME/.config/zsh"
+ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
+ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
